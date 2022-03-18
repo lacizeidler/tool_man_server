5452b5bc9fe95885e22e27cb21364be95750d10c
lacizeidler <laci.zeidler@gmail.com>
2022-03-18 15:03:48 -0500

    url tuple update

--- tool_man/settings.py
+++ tool_man/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://tool-man-server.herokuapp.com/'
+    'https://tool-man-client.herokuapp.com/'
 )
 
 MIDDLEWARE = [
